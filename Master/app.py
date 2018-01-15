f747ecd8484a158396ff96359ee9e0760e8f39be
Tegark <34651767+Tegark@users.noreply.github.com>
2018-01-15 23:56:55 +0300

    Patch 1.01
    - Added volume to db
    - Added GET and POST methods to Master
    - Added pipeline to worker
    - Added ping database before starting to calculate

--- Master/app.py
+++ Master/app.py
@@ -5,29 +5,42 @@ app = Flask(__name__)
 
 
 
-@app.route('/')
+@app.route('/', methods = ['GET'])
 def index():
+        
+
+	fibNum = int(dbConn.lindex("fibNum", 0))
+	m = "Current Fibonacci num  = " + str(fibNum) + " ."
+	return m
+
+@app.route('/', methods = ['POST'])
+def increment():
+        
         #Establishing container context by initializing sockets
 	context = zmq.Context()
 	socket = context.socket(zmq.REQ)
 	socket.connect("tcp://worker:5555")
 
-	#Initializing connection to database
-        dbConn = redis.Redis('redis_db')
-
-        #Appending first numbers to database
-        if(int (dbConn.llen("fibNum")) == 0):
-                dbConn.rpush("fibNum", 2, 1)
-        
-	fibNum = int(dbConn.lindex("fibNum", 0))
-	m = "Current Fibonacci num  = " + str(fibNum) + " ."
-
         #Updating Fibonacci number
 	socket.send('poking workers...')	
 	curFib = socket.recv()
-        dbConn.lset("fibNum", 0, curFib)
-	dbConn.lset("fibNum", 1, fibNum)
-	return m
+        #dbConn.lset("fibNum", 0, curFib)
+	#dbConn.lset("fibNum", 1, fibNum)
+
+	return "Increase successful"
+	
 
 if __name__ == "__main__":
+
+        #Initializing connection to database
+        dbConn = redis.Redis('redis_db')
+        try:
+                dbConn.ping()
+        except redis.ConnectionError:
+                print('Connection to database is not established')
+        
+         #Appending first numbers to database
+        if(int (dbConn.llen("fibNum")) == 0):
+                dbConn.rpush("fibNum", 2, 1)
+     
         app.run(host="0.0.0.0", debug=True)
--- Worker/app.py
+++ Worker/app.py
@@ -1,28 +1,53 @@
 import redis
 import zmq 
 
-def calcFib():
-        #Establishing connection to database
-	dbConn = redis.Redis('redis_db')
+def calcFib(dbConn):
 
-	#Getting last fibonacci number
-	fibNum1 = int(dbConn.lindex("fibNum", 1))
+        pipe = dbConn.pipeline()
+        while 1:
+                try:
+                        print('TRYING TO CALCULATE')
+                        pipe.watch("fibNum")
+                        #Establishing connection to database
+                        #dbConn = redis.Redis('redis_db')
 
-	#Getting current fibonacci number 
-	fibNum2 = int(dbConn.lindex("fibNum", 0))
+                        #Getting last fibonacci number
+                        fibNum1 = int(pipe.lindex("fibNum", 1))
 
-	sumFib = fibNum1 + fibNum2
-	return str(sumFib)
+                        #Getting current fibonacci number 
+                        fibNum2 = int(pipe.lindex("fibNum", 0))
 
-# Binding worker to net context 
+                        sumFib = fibNum1 + fibNum2
+
+                        print('CALCULATED')
+                        
+                        pipe.lset("fibNum", 0, sumFib)
+                        pipe.lset("fibNum", 1, fibNum2)
+
+                        pipe.execute()
+                        break
+
+                except redis.WatchError:
+                        continue
+                finally:
+                        pipe.reset()
+                        #return str(sumFib)
+        return
+
+# Binding worker to net context
+print('BIND WORKER')
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
 
+dbConn = redis.Redis('redis_db')
+print('BINDED')
 while True:
         #Receiving poke from master
 	poke = socket.recv()
-
+        print('RECEIVED POKE FROM MASTER')
 	#Updating fibonacci number 
-	curFib = calcFib()
-	socket.send(curFib)
+	calcFib(dbConn)
+	print('METHOD calcFib executed')
+	socket.send('New Fibonacci number calculated')
+	print ('SENT ENDMESSAGE')
